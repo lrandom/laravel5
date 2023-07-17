e8bdb54008308af412eee294f0025201243ec1e1
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-07-17 20:21:27 +0700

    update soft delete

--- routes/demo_session_4.php
+++ routes/demo_session_4.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Category;
 
 Route::get('/category', function () {
-    $categories = Category::where('id', '>', 20)->get();
+    $categories = Category::onlyTrashed()->get();
     foreach ($categories as $category) {
         echo $category->id;
     }
@@ -17,13 +17,31 @@ Route::get('/categoryById', function () {
 });
 
 Route::get('insert-category', function () {
-   /* $category = new Category();
-    $category->name = 'Áo nam 19';
-    $category->parent_id = 0;
-    $category->save();*/
+    /* $category = new Category();
+     $category->name = 'Áo nam 19';
+     $category->parent_id = 0;
+     $category->save();*/
+
     Category::create([
         'name' => 'Áo nam 20',
         'parent_id' => 0
     ]);
 });
+
+Route::get('add-category-form', function () {
+    return view('category.add');
+});
+
+Route::post('handle-category-form', function () {
+    Category::create(request()->all());
+})->name('category.handle.add');
+
+Route::get('delete-category', function () {
+    Category::where('id', '>', 10)->delete();
+});
+
+Route::get('revert-delete-category', function () {
+    Category::where('id', '>', 10)->restore();
+});
+
 ?>
