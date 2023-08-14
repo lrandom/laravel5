feb81f2c7985ab64d6784b335fe78b4a09a08d7d
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-08-14 19:46:33 +0700

    update login

--- app/Http/Middleware/CheckIsDesktopMiddleware.php
+++ app/Http/Middleware/CheckIsDesktopMiddleware.php
@@ -22,6 +22,4 @@ class CheckIsDesktopMiddleware
         }
         return $next($request);
     }
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-require_once __DIR__ . '/admin.php';
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/chrome', function () {
-    echo "Chrome Only content";
-})->middleware([\App\Http\Middleware\CheckIsDesktopMiddleware::class]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get("/chrome-2", function () {
-    echo "Chrome Only content";
-})->middleware([\App\Http\Middleware\CheckIsDesktopMiddleware::class]);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get("/warning-browser", function () {
-    echo "Ban chi co the truy cap bang trinh duyet Chrome";
-})->name("mobile.warning");
+Route::get('/my-profile', function () {
+    $authUser = \Illuminate\Support\Facades\Auth::user();
+    return view('user.my-profile', compact('authUser'));
+})->middleware(['auth'])->name('my-profile');
+
+
+require __DIR__ . '/auth.php';
