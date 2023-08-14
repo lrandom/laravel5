373c409d502a27165b6d1574de0e997c6eddebc4
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-08-14 19:00:30 +0700

    demo middleware

--- app/Http/Middleware/CheckIsDesktopMiddleware.php
+++ app/Http/Middleware/CheckIsDesktopMiddleware.php
@@ -5,20 +5,23 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use hisorange\BrowserDetect\Parser as Browser;
 
 class CheckIsDesktopMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
     public function handle(Request $request, Closure $next): Response
     {
         //check if ma la mobile
-        if (isMobile()) {
+        if (!Browser::isChrome()) {
             return redirect()->route('mobile.warning');
         }
         return $next($request);
     }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 require_once __DIR__ . '/admin.php';
+
+Route::get('/chrome', function () {
+    echo "Chrome Only content";
+})->middleware([\App\Http\Middleware\CheckIsDesktopMiddleware::class]);
+
+Route::get("/chrome-2", function () {
+    echo "Chrome Only content";
+})->middleware([\App\Http\Middleware\CheckIsDesktopMiddleware::class]);
+
+Route::get("/warning-browser", function () {
+    echo "Ban chi co the truy cap bang trinh duyet Chrome";
+})->name("mobile.warning");
